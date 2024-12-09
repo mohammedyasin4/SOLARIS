70aad15762d898c1de77f409c9b0e69f4b73dedf
mohammedyasin4 <leonahmad4@gmail.com>
2024-12-09 14:05:00 +0100

    jag skrev kommentarar till java script filen

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
-var planets;
+var planets; // vi deklarerar variablen planet 
 
 let focusedPlanet = null;
 
 let planetsColors = [
     'yellow', // SOl
-    '#8b8983',
-    '#e6cbca',
-    '#418ed5',
+    '#8b8983', // Merkurius
+    '#e6cbca', // Venus
+    '#418ed5', // Jorden
     '#eb5c5d',
     '#e29468',
     '#c7aa72',
@@ -15,92 +15,91 @@ let planetsColors = [
     '#7a91a7'
 ];
 
-async function getApiData(){
+async function getApiData(){ // Asynkron funktion för att hämta data från ett API
     let resp = await fetch('https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/keys', {
-        method: 'POST',
+        method: 'POST', // Väntar på svaret från API:et efter att ha skickat en POST
         headers: {'x-zocom': 'solaris-2ngXkR6S02ijFrTP'}
     })
-    let data = await resp.json();
+    let data = await resp.json(); // Nu väntar på att svaret ska konverteras till JSON format
    
-    let bodies = await fetch('https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies',
+    let bodies = await fetch('https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies', // Väntar på att hämta data från API som returnerar information om "bodies"
         {
-            method: 'GET',
+            method: 'GET', // Definierar alternativ för fetch-anropet för att hämta data
             headers: {'x-zocom': data.key}
         }
     )
-    let result = await bodies.json();
+    let result = await bodies.json(); // 'result' kommer nu att innehålla den hämtade informationen från API:et
     planets = result.bodies;
 }
 
-async function generatePlanets() {
+async function generatePlanets() { // Denna funktion kommer att hämta information om planeter från ett API
     const solarSystem = document.getElementById('solarSystem');
 
-    // Get API key then data of planets
-    await getApiData();
+    await getApiData(); // Väntar på att hämta data från API:et
 
     let distanceBetweenPlanets = 0;
 
-    // Generate planets and orbits
+    // Itererar över varje planet i planets-arrayen
     planets.forEach((planet, index) => {
         if(index == 0){
-            return; // vi skippar solen eftersom den ritas i css filen 
+            return; // vi skippar solen eftersom den ritas i CSS filen 
         }
         if(index == 4){
             planet.distance = 228000000; // API visar fel distans för planeten Mars
         }
    
         
-        const planetContainer = document.createElement('div');
+        const planetContainer = document.createElement('section');
         planetContainer.className = 'planet-container';
         planetContainer.style.zIndex = planets.length - index; 
         
-        const planetElement = document.createElement('div');
+        const planetElement = document.createElement('section');// Skapar ett nytt HTML-element av typen 'section'
         planetElement.className = 'planet';
-        planetElement.style.width = (planet.circumference / planets[0].circumference) * 1500 + 'px'; // planetens omkrets / solens omkrets
-        planetElement.style.height = (planet.circumference / planets[0].circumference) * 1500 + 'px';
-        planetElement.style.backgroundColor = planetsColors[index];
-        planetElement.style.top = '-1rem';
-        if(planet.name=='Merkurius'){
-            distanceBetweenPlanets = 300; // px
-            planetElement.style.left = distanceBetweenPlanets + 'px';
+        planetElement.style.width = (planet.circumference / planets[0].circumference) * 1500 + 'px'; // Skalar omkretsen i förhållande till solens omkrets för att sätta bredden i pixlar
+        planetElement.style.height = (planet.circumference / planets[0].circumference) * 1500 + 'px'; // Skalar omkretsen i förhållande till solens omkrets för att sätta höjden i pixlar
+        planetElement.style.backgroundColor = planetsColors[index]; // Sätter bakgrundsfärgen för planetElement baserat på planetens färg
+        planetElement.style.top = '-1rem'; // Sätter den vertikala positionen för planetElement till -1rem
+        if(planet.name=='Merkurius'){ // Kontrollerar om den aktuella planeten är Merkurius
+            distanceBetweenPlanets = 300; 
+            planetElement.style.left = distanceBetweenPlanets + 'px'; 
         }
         else{
-            planetElement.style.left = distanceBetweenPlanets + 'px';
+            planetElement.style.left = distanceBetweenPlanets + 'px'; 
         }
         distanceBetweenPlanets += 185;
 
-       planetElement.addEventListener('click', (event) => {
-            event.stopPropagation();
+       planetElement.addEventListener('click', (event) => { // Lägger till en klick-händelselyssnare på planetElement
+            event.stopPropagation();  // Stoppar händelsepropagering så att klicket inte påverkar andra element
             handlePlanetClick(planet.name);
         });
-
-        planetContainer.appendChild(planetElement);
-        solarSystem.appendChild(planetContainer);
+ 
+        planetContainer.appendChild(planetElement); // Lägger till planetElement som ett barn till planetContainer
+        solarSystem.appendChild(planetContainer); // Detta placerar hela planetcontainern inom solsystemets layout
     });
 }
 
 
 
-function handlePlanetClick(planetName) {
+function handlePlanetClick(planetName) { // Funktion för att hantera klick på en planet
     if (planetName){focusedPlanet = planetName;
     }else{
         
             focusedPlanet = null;
-            document.getElementById('searchInput').value = ''; // Clear the search input
+            document.getElementById('searchInput').value = '';
         
     }
 
     updatePlanetFocus();
 }
 
-function updatePlanetFocus() {
+function updatePlanetFocus() { // Funktion för att uppdatera informationen om den fokuserade planeten
     const focusedPlanetInfo = document.getElementById('focusedPlanetInfo');
     const planetElements = document.querySelectorAll('.planet');
 
     if (focusedPlanet) {
-        const planet = planets.find(p => p.name === focusedPlanet);
+        const planet = planets.find(p => p.name === focusedPlanet); // Söker efter den planet som matchar namnet på den fokuserade planeten
         document.getElementById('focusedPlanetName').textContent = planet.name;
-        document.getElementById('focusedPlanetDescription').textContent = planet.desc;
+        document.getElementById('focusedPlanetDescription').textContent = planet.desc; // Uppdaterar innehållet i elementet som visar beskrivningen av den fokuserade planeten
         focusedPlanetInfo.style.display = 'grid';
         setTimeout(() => focusedPlanetInfo.classList.add('visible'), 10);
 
@@ -120,22 +119,22 @@ function updatePlanetFocus() {
         });
     }
 }
-function searchPlanet() {
-    const searchInput = document.getElementById('searchInput').value.toLowerCase();
-    const planet = planets.find(p => p.name.toLowerCase() === searchInput);
+function searchPlanet() { // Funktion för att söka efter en planet 
+    const searchInput = document.getElementById('searchInput').value.toLowerCase(); // Hämtar värdet från sökfältet med id 'searchInput'
+    const planet = planets.find(p => p.name.toLowerCase() === searchInput); // Söker efter planeten i planets-arrayen vars namn matchar söksträngen
 
     if (planet) {
-        focusedPlanet = planet.name; // Set the focused planet
-        updatePlanetFocus(); // Update the display
+        focusedPlanet = planet.name;     // Om en matchande planet hittas, uppdateras focusedPlanet med planetens namn
+        updatePlanetFocus();
     } else {
-        focusedPlanet = null; // Reset if no match
-        updatePlanetFocus(); // Update the display
+        focusedPlanet = null; 
+        updatePlanetFocus(); 
     }
 }
 
-function checkEnter(event) {
+function checkEnter(event) { // Syftet är att reagera på Enter-tangenten för att söka efter en planet
     if (event.key === 'Enter') {
-        searchPlanet(); // Call the search function when Enter is pressed
+        searchPlanet(); 
     }
 }
 
